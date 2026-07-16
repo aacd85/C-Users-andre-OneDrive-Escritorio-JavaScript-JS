5a1bc11663d3663e4690c2361a8a54bfd6b5fa63
Andrea Carranza <andreacarranza85@gmail.com>
2026-07-16 18:39:59 -0500

    Corrige estructura del proyecto

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+let nombre="Emiliano";
+let apellido="Uribe"
+
+let edad= parseInt(prompt("Ingresa tu edad"))
+console.log (typeof edad)
+
+console.log (nombre + " " + apellido)
+
+console.log (edad)
+
+console.log (2026 - edad)
+
+console.log (2026 - parseInt(edad))
+
+let resultado= 2026 - edad 
+console.log ("Emiliano tiene " + edad + " " + "años porque nació en el año " + resultado )
